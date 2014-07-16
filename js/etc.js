42b4531b3d4e5516f2909655e1617c9be8f068f1
f1vlad <kulchitski@gmail.com>
2014-07-16 01:30:05 -0400

    Add slider

---
+++ js/etc.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.slickjs').slick({
+      autoplay: true,
+      autoplaySpeed: 6000,
+      fade: true,
+      easing: 'linear',
+      infinite: true,
+      pauseOnHover: true,
+      speed: 2600,
+      swipe: true,
+      touchMove: true,
+      arrows: false
+    });
+});
